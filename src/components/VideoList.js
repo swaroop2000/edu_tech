8db58ae64b400ec89b5319d4a7126f3a2000e00b
cesiumtech <shanthiswaroop2000@gmail.com>
2024-06-15 13:55:00 -0700

    videos count

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -409,8 +409,30 @@ const VideoList = ({ userId, onVideoSelect }) => {
     <div className="flex-grow px-4 overflow-auto">
       <div className="bg-indigo-900 py-4 px-4">
         <h2 className="text-white text-5xl font-black uppercase text-center mb-4">Your Videos</h2>
+        
+        <div class="mt-12">
+      <div class="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
+        <div class="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
+          <div class="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-blue-600 to-blue-400 text-white shadow-blue-500/40 shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="w-6 h-6 text-white">
+              <path d="M12 7.5a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"></path>
+              <path fill-rule="evenodd" d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 011.5 14.625v-9.75zM8.25 9.75a3.75 3.75 0 117.5 0 3.75 3.75 0 01-7.5 0zM18.75 9a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V9.75a.75.75 0 00-.75-.75h-.008zM4.5 9.75A.75.75 0 015.25 9h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H5.25a.75.75 0 01-.75-.75V9.75z" clip-rule="evenodd"></path>
+              <path d="M2.25 18a.75.75 0 000 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 00-.75-.75H2.25z"></path>
+            </svg>
+          </div>
+          <div class="p-4 text-right">
+            <p class="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">Total Videos</p>
+            <h4 class="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">{videos.length}</h4>
+          </div>
+          </div>
+        </div>
+        </div>
+
+
+
+
         <div className="flex justify-between items-center mb-4">
-          <p className="text-white">Number of Videos: {videos.length}</p>
+          {/* <p className="text-white">Number of Videos: {videos.length}</p> */}
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Add New Video
           </button>
